bf815378962e4cf62f80b141adbb223250b591e8
Maciej Byczko <byczqq136@gmail.com>
2022-04-10 22:08:37 +0200

    Add mail
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- lab03/src/components/Students.js
+++ lab03/src/components/Students.js
@@ -10,9 +10,7 @@ const Student = (props) => {
   };
 
   const HandleChange = (event) => {
-    // console.log(event.target.value)
     setOptionState(event.target.value);
-    // optionState = event.target.value;
   };
 
   const GenerateList = (tags) => {
@@ -31,50 +29,44 @@ const Student = (props) => {
   };
 
   const StudentsQueryHTML = students
-    .filter((it) => 
-    // {
-      (optionState === "desc") ? it.desc.includes(studentsQuery) : 
-      (optionState === "tags") ? it.tags.some(el => el.includes(studentsQuery)) : 
-      (optionState === "subjects") ? it.subjects.some(el => el.includes(studentsQuery)) : true
-      // switch (optionState) {
-      //   case "desc":
-      //     return it.desc.includes(studentsQuery);
-      //   case "tags":
-      //     return it.tags.includes(studentsQuery);
-      //   case "subjects":
-      //     return it.subjects.includes(studentsQuery);
-      // }
-    // }
-      )
+    .filter((it) =>
+      optionState === "desc"
+        ? it.desc.includes(studentsQuery)
+        : optionState === "tags"
+        ? it.tags.some((el) => el.includes(studentsQuery))
+        : optionState === "subjects"
+        ? it.subjects.some((el) => el.includes(studentsQuery))
+        : true
+    )
     .map((it, i) => {
       return (
-        // <p >
         <ul key={i} className="ads">
-          <li><h3>Name</h3> {it.name}</li>
-          <li><h3>Description</h3> {it.desc}</li>
-          <li><h3>Tags</h3> {GenerateList(it.tags)}</li>
-          <li><h3>Subjects</h3> {GenerateList(it.subjects)}</li>
+          <li>
+            <h3>Name</h3> {it.name}
+          </li>
+          <li>
+            <h3>Description</h3> {it.desc}
+          </li>
+          <li>
+            <h3>Tags</h3> {GenerateList(it.tags)}
+          </li>
+          <li>
+            <h3>Subjects</h3> {GenerateList(it.subjects)}
+          </li>
         </ul>
-        // </p>
       );
     });
 
-  // const toDosListHTML = studentQuery.map((it, i) => {
-  //   return <p key={i}>{it}</p>;
-  // });
-
   return (
     <div className="app">
-      <h1>Student Page Body</h1>
+      <h1>Students without friends</h1>
       <select value={optionState} onChange={HandleChange}>
         <option value="tags">Tags</option>
         <option value="subjects">Subjects</option>
         <option value="desc">Description</option>
       </select>
       <input type="text" value={studentsQuery} onChange={HandleQueryChange} />
-      <div className="content">
-      {StudentsQueryHTML}
-      </div>
+      <div className="content">{StudentsQueryHTML}</div>
     </div>
   );
 };
